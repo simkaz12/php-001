bca79f94610bd9a50dfdeb81d3a6d4521f5e08d0
Simonas Kazlauskas <sksimkaz8@gmail.com>
2023-06-29 14:17:36 +0300

    react

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+console.clear();
+
 //console.log('helliau');
 
 //let a = 10;
@@ -66,7 +68,7 @@ console.log(fun2(2));
 console.log(fun3(3));
 console.log(fun4(4));
 fancy();
-*/
+
 
 const nicePrint = (a, b, fun) => {
     const p = fun(a, b);
@@ -126,17 +128,86 @@ const map = colorsAnimal.map((house, _) => [house.color, house.name]);
 console.log(fe);
 console.log(map);
 
+let a;
+let b;
+
+a = 5;
+b = a; // by value
+
+a++;
+
+console.log(a, b);
+let c;
+let d;
+
+c = [5];
+d = c; // by reference
+
+d = [...c]; // copy by value     ... => spreadinam visas reiksmes ir idedam i obj (),[],{}.
+
+c.push(6);
+d.push(7);
+
+console.log(c, d);
+
+e = {value: 5, name: 'jonas', animal: 'dog'};
+f = e; // by ref
+
+f = {...e, name: 'simas'}; // copy by value plus name change
+
+e.color = 'red';
+
+console.log('e:', e, 'f:', f);
 
 
+const g = [1, 2, 3, 4, 5, 6];
+
+const [first, second] = g;
+
+console.log(first, second);
+
+const h = {value:5, name: 'jonas', animal: 'iguana'};
+
+const {value, animal} = h;
+
+console.log(value, animal);
+
+*/
+
+const house = [
+    {id: 2, name: 'jonas', animal: 'dog', color: 'red', price: 1000},
+    {id: 43, name: 'simas', animal: 'cat', color: 'green', price: 4000},
+    {id: 22, name: 'ona', animal: 'dragon', color: 'blue', price: 500},
+    {id: 8, name: 'petras', animal: 'snake', color: 'teal', price: 7800},
+    {id: 7, name: 'gedas', animal: 'racoon', color: 'black', price: 400},
+    {id: 153, name: 'zenius', animal: 'boar', color: 'yellow', price: 4800},
+]
 
 
+// pretend that house is immutable
+// make cats yellow
 
+const newHouse = house.map(m => m.animal === 'cat' ? {...m, color: 'yellow'} : {...m});
 
+const ownRacoon = house.filter(m => m.animal === 'racoon');
 
+const getSimas = house.find(m => m.name === 'simas');
 
+const killJonas = house.filter(m => m.name !== 'jonas');
 
+// sort by price
 
+const sortHouse = [...house].sort((a, b) => a.price - b.price);
 
+// sort by name
+
+const sortByName = [...house].sort((a, b) => a.name.localeCompare(b.name));
+
+// get all racoon owners and by name
+
+const allRacoons = house.filter(m => m.animal === 'racoon').sort((a, b) => a.name.localeCompare(b.name));
+
+console.log(sortByName, house);
 
 
 
